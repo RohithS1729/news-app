5032dcb6bc10bbcb1c57ef638e9f626fb7d65b24
RohithS1729 <rohi741@gmail.com>
2023-03-08 18:44:36 +0530

    backend-setup

---
+++ ExpressJs-Back-End/server.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const dotenv=require("dotenv")
+dotenv.config()
+const app=express();
+app.use(cors())
+app.use(bodyParser.json())
+
+
+//dbms
+mongoose.connect(process.env.CONNECTION_STRING);
+const db= mongoose.connection;
+db.on('error',()=>{console.log('did not connect to db')});
+db.on('open',()=>{console.log('started listening to db')});
+
+app.get("/test",(req,res)=>{
+    res.send("working")
+})
+
+app.listen(8000,()=>{
+    console.log('listening')
+})
